2cfd78b791a464a565b4bec8c91a4ecc607f7b5f
risapav <risapav@gmail.com>
2022-02-09 21:53:01 +0100

    fnk

--- src/main.py
+++ src/main.py
@@ -27,30 +27,56 @@ def usage():
 #        crc &= 0xFF
 #    return crc
 
-def calc_sum(datagram, csum):
+def calc_sum(buffer, csum):
     sum = csum
     # Iterate bytes in data
-    for byte in datagram:    
+    for byte in buffer:    
         sum += byte
+#        sum ^= byte
     return sum & 0xFF
 
-def calc_crc(csum):
-    crc = 0x100 - csum 
+def calc_crc(csum, sum):
+    crc = 0x100 - (csum -sum) & 0xFF
+  #  crc = - (csum - sum)
+ #   crc = csum - sum
     return crc & 0xFF
  #   return csum & 0xFF
 
-def statistic(ids, a_size, a_sum, b_size, b_sum):
-    print (f"records total:\t{ids}\n")
+def statistic(a_size, a_sum, b_size, b_sum):
     print (f"VBT file size:\t{a_size}\t\tcomputed file size:\t{b_size}\n")
     print (f"VBT file sum:\t{a_sum}\t\tcomputed file sum:\t{b_sum}\n")
     print ()
 
+def readData(size, b_sum, b_size, file):
+    retval = 0
+    buffer = file.read(size)
+    
+    if len(buffer) != size: 
+        retval = 1
+
+    return (retval, calc_sum(buffer, b_sum), b_size + size, buffer)
+
+def writeData(buffer, size, b_sum, b_size, file):
+    retval = 0
+    file.write(buffer)
+    
+    #if len(buffer) != size: 
+    #    retval = 1
+
+    return (retval, calc_sum(buffer, b_sum), b_size + size)  
+
+
 def decompose(filename):
     records = []
-    vbt_size = 0
-    vbt_sum = 0
-    filesize = 0  
-    csum = 0
+    v_size = 0
+    v_sum = 0
+    
+    f_size = 0  
+    f_sum = 0
+    
+    b_size = 0
+    b_sum = 0
+
     index = 0
     try:
         binfile = ""
@@ -66,30 +92,31 @@ def decompose(filename):
 
             ################################
             # VBT header should be the first
-            data_p = f.read(VBT.vbt_header_size)
-            if not data_p: 
+            retval, f_sum, f_size, data_p = readData(VBT.vbt_header_size, f_sum, f_size, f)
+            if retval:
                 print ("nothing to do")
-                return 1  
-            filesize += VBT.vbt_header_size
-            csum = calc_sum(data_p, csum)
-           
+                return 1 
+      
             data_u = VBT.vbt_header_unpack(data_p)
             if VBT.check_VBT_header(data_u):
                 print ("nie je VBT súbor")
                 return 1  
             records.append( str(data_u) )
             index += 1
-            vbt_size = data_u[3]
-            vbt_sum = data_u[4]
+            v_size = data_u[3]
+            v_sum = data_u[4]
             
+            statistic(v_size, v_sum, f_size, calc_crc(f_sum, v_sum))
+
             ################################
             # BDB header should be the second
-            data_p = f.read(VBT.bdb_header_size)
-            if not data_p: 
+            retval, f_sum, f_size, data_p = readData(VBT.bdb_header_size, f_sum, f_size, f)
+            if retval:
                 print ("nothing to do")
-                return 1     
-            filesize += VBT.bdb_header_size
-            csum = calc_sum(data_p, csum)
+                return 1 
+
+            b_size += VBT.bdb_header_size
+            b_sum = calc_sum(data_p, b_sum)
 
             data_u = VBT.bdb_header_unpack(data_p)
             if VBT.check_BDB_header(data_u) :
@@ -99,26 +126,33 @@ def decompose(filename):
             sig, ver, h_size, size = VBT.bdb_header_unpack(data_p)    
             index += 1
 
+            statistic(v_size, v_sum, f_size, calc_crc(f_sum, v_sum))
+
             ################################
             # BDB blocks
             while True:
-                data_p = f.read(VBT.bdb_block_size)
-                if not data_p: 
+                retval, f_sum, f_size, data_p = readData(VBT.bdb_block_size, f_sum, f_size, f)
+                if retval:
                     break
-                filesize += VBT.bdb_block_size
-                csum = calc_sum(data_p, csum)
+
+                b_size += VBT.bdb_block_size
+                b_sum = calc_sum(data_p, b_sum)
+
                 id, size = VBT.bdb_block_unpack(data_p)
-                data_p = f.read(size)
-                if not data_p: 
+
+                retval, f_sum, f_size, data_p = readData(size, f_sum, f_size, f)
+                if retval:
                     break
-                filesize += size
-                csum = calc_sum(data_p, csum)
+
+                b_size += size
+                b_sum = calc_sum(data_p, b_sum)                
                 #records.append((id, size, str(binascii.hexlify(data_p))))
                 records.append(str((id, size, binascii.hexlify(data_p))))
                 index += 1
             
         # prepare statistics
-        statistic(index, vbt_size, vbt_sum, filesize, calc_crc(csum))
+        statistic(v_size, v_sum, f_size, calc_crc(f_sum, v_sum))
+
     except FileNotFoundError:
         msg = "Sorry, the file "+ binfile + " does not exist."
         print(msg) 
@@ -135,11 +169,11 @@ def decompose(filename):
     return 0
 
 def compose(filename):
-    vbt_size = 0
-    vbt_sum = 0
-    filesize = 0  
-    csum = 1
-    index = 0
+    v_size = 0
+    v_sum = 0
+    f_size = 0  
+    f_sum = 0
+
     try: 
         jsonfile = ""
         # load json file
@@ -158,8 +192,8 @@ def compose(filename):
 
     try:
         binfile = filename + ".new.vbt"
-        with open(binfile, "wb") as of:
-            for record in records:
+        with open(binfile, "wb") as ofile:
+            for index, record in enumerate(records):
                 ################################
                 # VBT header magic
                 if index == 0: 
@@ -168,18 +202,15 @@ def compose(filename):
                         print ("nie je VBT súbor: " + data_u[0][0:4])
                         break
                     else:
-                        vbt_size = data_u[3]
-                        vbt_sum = data_u[4]
-
-                    #    idx = 4
-                    #    data_u = data_u[:idx] + (0,) + data_u[idx + 1:]
-                        #data_u[4] = 0                       
+                        #v_size, v_sum = *data_u[3]
+                        #vbt_filesize
+                        v_size = data_u[3]
+                        #vbt_checksum
+                        v_sum = data_u[4]
 
                         data_p = VBT.s_vbt_h.pack(*data_u)                        
-                        of.write(data_p)
-                        filesize += VBT.vbt_header_size
-                        csum = calc_sum(data_p, csum)
-                    index += 1
+                        retval, f_sum, f_size = writeData(data_p, VBT.vbt_header_size, f_sum, f_size, ofile)
+                    
                 ################################
                 # BDB header magic
                 elif index == 1: 
@@ -189,30 +220,29 @@ def compose(filename):
                         break
                     else:
                         data_p = VBT.s_dbd_h.pack(*data_u)
-                        of.write(data_p)
-                        filesize += VBT.bdb_header_size
-                        csum = calc_sum(data_p, csum)
-                    index += 1
+                        retval, f_sum, f_size = writeData(data_p, VBT.bdb_header_size, f_sum, f_size, ofile)
+
+                    f_sum = 0
+                    
                 ################################
                 # BDB block magic
                 else: 
                     data_u = eval(record)
                     id, size, block = data_u
                     data_p = VBT.s_bdb_b.pack(id, size) + binascii.unhexlify(block)
-                    of.write(data_p)
-                    filesize += VBT.bdb_block_size + size
-                    csum = calc_sum(data_p, csum)
-                    index += 1
+                    retval, f_sum, f_size = writeData(data_p, VBT.bdb_block_size + size, f_sum, f_size, ofile)
+                    
                     
     # prepare statistics
             data_u = eval(records[0])
             v_size = data_u[3]
             v_sum = data_u[4]
-            statistic(index, vbt_size, vbt_sum, filesize, calc_crc(csum))
+            statistic(v_size, v_sum, f_size, calc_crc(f_sum, v_sum))
+
     # update vbt header        
             data_p = VBT.sz_cs.pack(v_size, v_sum)
-            of.seek(24,0)
-            of.write(data_p)
+            ofile.seek(24,0)
+            ofile.write(data_p)
     except:
         msg = "Sorry, the file " + binfile + " is not writable."
         print(msg) 
